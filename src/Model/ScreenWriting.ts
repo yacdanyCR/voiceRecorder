b69f062c8126d2d6ce88e5fc23e69dd147a040d6
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2023-05-26 18:02:21 -0600

    Showing the information in the html

--- src/Model/ScreenWriting.ts
+++ src/Model/ScreenWriting.ts
@@ -5,15 +5,31 @@ class ScreenWriting {
 		const div_writing__Section = document.createElement("div") as HTMLDivElement
 		const div_writing__Section_place = document.createElement("div") as HTMLDivElement
 		const div_recordIcon = document.createElement("div") as HTMLDivElement
+		const iconStartMicrophone = document.createElement("img") as HTMLImageElement
+		const iconStopMicrophone = document.createElement("img") as HTMLImageElement
 
 		$app.append(section)
 		div_writing__Section.className = "writing__Section"
 		section.append(div_writing__Section)
-		div_writing__Section_place.className = "writing__Section--Place"
+		div_writing__Section_place.className = "writing__Section--Place paper container"
 		div_writing__Section.append(div_writing__Section_place)
 
 		div_recordIcon.className = "writing_Section--Icon"
 		div_writing__Section.append(div_recordIcon)
+
+		iconStartMicrophone.className = "writing__Record__Section"
+		iconStartMicrophone.alt = "icon"
+		iconStartMicrophone.src = "/start_microphone.svg"
+		iconStartMicrophone.width = 70
+
+		iconStopMicrophone.className = "writing__StopRecord__Section"
+		iconStopMicrophone.alt = "icon"
+		iconStopMicrophone.src = "/stop_microphone.svg"
+		iconStopMicrophone.width = 70
+		iconStopMicrophone.hidden = true
+
+		div_recordIcon.append(iconStartMicrophone)
+		div_recordIcon.append(iconStopMicrophone)
 	}
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
 import { ScreenWriting } from "./Model/ScreenWriting"
 
 ScreenWriting.render()
+const $startRecording = document.querySelector(".writing__Record__Section") as HTMLImageElement
+const $stopRecording = document.querySelector(".writing__StopRecord__Section") as HTMLImageElement
+const $writing__Section = document.querySelector(".writing__Section--Place") as HTMLDivElement
 
 const voiceRecord = new webkitSpeechRecognition()
 voiceRecord.lang = "es-Es"
 voiceRecord.interimResults = false
 voiceRecord.continuous = true
 
-voiceRecord.start()
+
+$startRecording.addEventListener("click", () => {
+  $startRecording.hidden = true
+  $stopRecording.hidden = false
+
+  voiceRecord.start()
+})
+
+$stopRecording.addEventListener("click", () => {
+  $startRecording.hidden = false
+  $stopRecording.hidden = true
+  voiceRecord.abort()
+})
 
 voiceRecord.onresult = (ev: SpeechRecognitionEvent) => {
-  console.log(ev.results[ev.results.length - 1][0].transcript)
+  const data = ev.results[ev.results.length - 1][0].transcript
+
+  $writing__Section.innerHTML += `<p>${data}</p>`
 }
